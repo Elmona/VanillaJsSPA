055499c125ba1d6b82e54b1a2dc88b7adfa91f26
Emil Larsson <el223nc@student.lnu.se>
2017-12-17 12:05:24 +0100

    test

--- src/js/InstaChat.js
+++ src/js/InstaChat.js
@@ -24,14 +24,24 @@ class InstaChat {
         resolve()
         return
       }
+
       console.log(`Connecting to chat server at ${config.address}`)
+
       try {
         this.socket = new window.WebSocket(config.address)
       } catch (e) {
         reject(e)
       }
+
       this.socket.addEventListener('open', () => {
+        let data = {username: 'Server', data: 'Connected.'}
+        this.printMessage(data)
         resolve()
+      })
+
+      this.socket.addEventListener('onerror', e => {
+        let data = {username: 'Server', data: e}
+        this.printMessage(data)
       })
 
       this.socket.addEventListener('message', e => {
@@ -47,23 +57,34 @@ class InstaChat {
     let data = {
       type: 'message',
       data: msg,
-      username: 'Emil',
+      username: 'Yoynk',
       key: config.key
     }
-    this.connect()
-      .then(() => {
-        this.socket.send(JSON.stringify(data))
-      })
+    this.connect().then(() => {
+      this.socket.send(JSON.stringify(data))
+    })
   }
 
   printMessage (msg) {
+    let messages = document.querySelectorAll('.messages')[0]
     let template = this.chatDiv.querySelectorAll('template')[0]
     let messageDiv = document.importNode(template.content, true)
 
     messageDiv.querySelectorAll('.text')[0].textContent = msg.data
-    messageDiv.querySelectorAll('.author')[0].textContent = msg.username
+    messageDiv.querySelectorAll('.author')[0].textContent = `${this.getTime()} ${msg.username}: `
     console.log(`${msg.username}:${msg.data}`)
     this.chatDiv.querySelectorAll('.messages')[0].appendChild(messageDiv)
+
+    // Scroll to bottom of chat window
+    messages.scrollTop = messages.scrollHeight
+  }
+  getTime () {
+    let date = new Date()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    if (hours < 10) hours = `0${hours}`
+    if (minutes < 10) minutes = `0${minutes}`
+    return `[${hours}:${minutes}]`
   }
 }
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,13 @@
 
 const InstaChat = require('./InstaChat')
 const Window = require('./Window')
+const Memory = require('./Memory')
 
 const chatContainer = document.querySelector('#chatContainer')
 let chat = new InstaChat(chatContainer)
+chat.connect()
 
 const window = new Window()
 
-chat.connect()
+let memory = new Memory(4, 4, 'memoryContainer')
+memory.generateGame()
