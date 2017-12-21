2207ef4dc7cc37503dadb9eb2f198a857c745518
Emil Larsson <el223nc@student.lnu.se>
2017-12-21 08:44:30 +0100

    Added new function to change nickname

--- src/js/Chat/app.js
+++ src/js/Chat/app.js
@@ -4,7 +4,7 @@ const config = require('../config.json')
 class Chat {
   constructor (div) {
     this.div = div
-    this.messages = []
+    this.nick = 'Emil'
   }
   init () {
     console.log('Init chat')
@@ -54,18 +54,32 @@ class Chat {
   }
 
   sendMessage (msg) {
-    let data = {
-      type: 'message',
-      data: msg,
-      username: 'Emil',
-      key: config.key
+    // Searching for commands
+    let re = ''
+    let chatMsg = {}
+    chatMsg.username = 'Chat'
+
+    re = /\/nick */
+    if (re.exec(msg) !== null) {
+      let nick = msg.replace(re, '')
+      this.nick = nick
+      chatMsg.data = `Changing nick to ${this.nick}`
+      this.printMessage(chatMsg)
+    } else {
+      let data = {
+        type: 'message',
+        data: msg,
+        username: this.nick,
+        key: config.key
+      }
+      this.connect().then(() => {
+        this.socket.send(JSON.stringify(data))
+      })
     }
-    this.connect().then(() => {
-      this.socket.send(JSON.stringify(data))
-    })
   }
 
   printMessage (msg) {
+    console.log(msg)
     let messages = this.div.querySelectorAll('.messages')[0]
     let template = this.div.querySelectorAll('template')[0]
     let messageDiv = document.importNode(template.content, true)
