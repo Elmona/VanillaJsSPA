0752e60c5dc29da58082c97dbc30b3883ad4bd51
Emil Larsson <el223nc@student.lnu.se>
2018-01-08 08:29:29 +0100

    Added documentation

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,11 @@
 
+/**
+ * The starting point of the application.
+ *
+ * @author Emil Larsson
+ * @version 1.0.0
+ */
+
 const WindowManager = require('./WindowManager')
 const windowManager = new WindowManager()
 
