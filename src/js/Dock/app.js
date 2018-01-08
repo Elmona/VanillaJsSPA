d4d11eed1e822d88c399152de8da19c83b7fa599
Emil Larsson <el223nc@student.lnu.se>
2018-01-08 09:15:41 +0100

    Added documentation

--- src/js/Dock/app.js
+++ src/js/Dock/app.js
@@ -1,10 +1,27 @@
+/**
+ * Module for Examination 3
+ * Dock
+ *
+ * @author Emil Larsson
+ * @version 1.0.0
+ */
 
+/**
+*  init - Add EventListener to start apps.
+*
+*  @param {Element} div - Reference to the div element.
+*  @param {Object} wm - Reference to WindowManager object
+*/
 const init = (div, wm) => {
-  console.log('IIINNNIIIITTT')
   div.addEventListener('click', e => startApp(e, wm))
-  // document.querySelector('.container').appendChild(div)
 }
 
+/**
+*  startApp - EventListener to start apps.
+*
+*  @param {Element} div - Reference to the div element.
+*  @param {Object} wm - Reference to WindowManager object
+*/
 const startApp = (e, wm) => {
   wm.startApp(e.target.dataset.appname)
 }
