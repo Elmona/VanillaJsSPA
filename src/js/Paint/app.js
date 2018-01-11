aacd2be8b24a97035373857616e41b5a3f0d0b6d
Emil Larsson <el223nc@student.lnu.se>
2018-01-11 13:59:36 +0100

    Added settings

--- src/js/Paint/app.js
+++ src/js/Paint/app.js
@@ -14,42 +14,40 @@ class Paint {
     this.paint = false
     this.line = []
     this.picture = []
-    this.mouseX
-    this.mouseY
+    this.mouseX = 0
+    this.mouseY = 0
+    this.yOffset = 110
+    this.brushSize = 5
+    this.color = '#000'
   }
 
   init () {
+    this.container.querySelector('.PaintColor').addEventListener('change', e => {
+      this.color = e.target.value
+    })
+    this.container.querySelector('.PaintRange').addEventListener('change', e => {
+      this.brushSize = e.target.value
+    })
     this.canvas.addEventListener('mousedown', e => {
-      console.log(this.canvas)
-
-      // let mouseX = e.clientX - this.container.offsetLeft
-      // let mouseY = e.clientY - this.container.offsetTop - 80
-      /*
-      this.context.beginPath()
-      this.context.arc(mouseX, mouseY + 18, 10, 0, 2 * Math.PI, true)
+      this.mouseX = e.clientX - this.container.offsetLeft
+      this.mouseY = e.clientY - this.container.offsetTop - this.yOffset
       this.picture.push({
-        type: 'dot',
-        x: mouseX,
-        y: mouseY + 18
+        x: this.mouseX,
+        y: this.mouseY
       })
-      this.context.fill()
-      */
       this.paint = true
     })
 
     this.canvas.addEventListener('mousemove', e => {
-      if (this.paint) {
-        this.mouse = e.clientX - this.container.offsetLeft
-        this.mouseY = e.clientY - this.container.offsetTop - 100
+      this.mouseX = e.clientX - this.container.offsetLeft
+      this.mouseY = e.clientY - this.container.offsetTop - this.yOffset
 
+      if (this.paint) {
         this.line.push({x: this.mouseX, y: this.mouseY})
         this.drawLine()
       }
 
-      this.mouseX = e.clientX - this.container.offsetLeft
-      this.mouseY = e.clientY - this.container.offsetTop - 100
-
-      this.container.querySelector('h6').textContent = `x: ${this.mouseX} y: ${this.mouseY}`
+      this.container.querySelector('h6').textContent = `x: ${this.pad(this.mouseX)} y: ${this.pad(this.mouseY)}`
     })
 
     this.canvas.addEventListener('mouseup', e => {
@@ -57,21 +55,22 @@ class Paint {
     })
   }
 
+  pad (num) {
+    if (num < 10) {
+      return '00' + num
+    } else if (num < 100) {
+      return '0' + num
+    } else {
+      return num
+    }
+  }
+
   drawLine () {
     this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height)
-    this.context.strokeStyle = '#000'
-    this.context.lineWidth = 10
+    this.context.strokeStyle = this.color
+    this.context.lineWidth = this.brushSize
     this.context.lineJoin = 'round'
-    /*
-    this.picture.forEach(x => {
-      if (x.type === 'dot') {
-        // console.log(x)
-        this.context.beginPath()
-        this.context.arc(x.x, x.y + 18, 10, 0, 2 * Math.PI, true)
-        this.context.fill()
-      }
-    })
-    */
+
     this.line.forEach((x, i) => {
       if (i > 0) {
         this.context.beginPath()
@@ -87,3 +86,23 @@ class Paint {
 }
 
 module.exports = Paint
+    /*
+    this.picture.forEach(x => {
+      if (x.type === 'dot') {
+        // console.log(x)
+        this.context.beginPath()
+        this.context.arc(x.x, x.y + 18, 10, 0, 2 * Math.PI, true)
+        this.context.fill()
+      }
+    })
+    */
+     /*
+      this.context.beginPath()
+      this.context.arc(mouseX, mouseY + 18, 10, 0, 2 * Math.PI, true)
+      this.picture.push({
+        type: 'dot',
+        x: mouseX,
+        y: mouseY + 18
+      })
+      this.context.fill()
+      */
