bb66cf222a0c8d3389100876826bd054db18ae46
Emil Larsson <el223nc@student.lnu.se>
2018-01-13 21:57:10 +0100

    Added cursor for canvas

--- src/js/Paint/app.js
+++ src/js/Paint/app.js
@@ -11,8 +11,8 @@ class Paint {
     this.container = div
     this.canvas = div.querySelector('canvas')
     this.context = this.canvas.getContext('2d')
+    this.cursor = div.querySelector('#cursor')
     this.paint = false
-    this.line = []
     this.picture = []
     this.mouseX = 0
     this.mouseY = 0
@@ -34,11 +34,11 @@ class Paint {
       this.brushSize = e.target.value
     })
 
-    this.canvas.addEventListener('mousedown', e => {
+    this.cursor.addEventListener('mousedown', e => {
       this.mouseX = e.clientX - this.container.offsetLeft
       this.mouseY = e.clientY - this.container.offsetTop - this.yOffset
 
-      // TODO: Make a dot the size of the brush
+      // A dot the size of the brush
       this.context.beginPath()
       this.context.fillStyle = this.color
       this.context.arc(this.mouseX, this.mouseY, this.brushSize / 2, 0, 2 * Math.PI, true)
@@ -60,19 +60,21 @@ class Paint {
       this.mouseX = e.clientX - this.container.offsetLeft
       this.mouseY = e.clientY - this.container.offsetTop - this.yOffset
 
+      // Change pointer
+      // console.log(this.cursor)
+      this.cursor.style.left = this.mouseX + 'px'
+      this.cursor.style.top = this.mouseY + this.yOffset + 'px'
+
       if (this.paint) {
         let x = this.picture.filter(x => x.id === this.id)
         x[0].line.push({x: this.mouseX, y: this.mouseY})
 
-        // this.line.push({x: this.mouseX, y: this.mouseY})
-        // this.drawLine()
         this.drawPicture()
       }
-
       this.container.querySelector('h6').textContent = `x: ${this.pad(this.mouseX)} y: ${this.pad(this.mouseY)}`
     })
 
-    this.canvas.addEventListener('mouseup', e => {
+    this.cursor.addEventListener('mouseup', e => {
       this.paint = false
     })
   }
@@ -105,24 +107,6 @@ class Paint {
     })
   }
 
-  drawLine () {
-    // this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height)
-    this.context.strokeStyle = this.color
-    this.context.lineWidth = this.brushSize
-    this.context.lineJoin = 'round'
-
-    this.line.forEach((x, i) => {
-      if (i > 0) {
-        this.context.beginPath()
-        let prevX = this.line[i - 1].x
-        let prevY = this.line[i - 1].y
-        this.context.moveTo(prevX, prevY)
-        this.context.lineTo(x.x, x.y)
-        this.context.closePath()
-        this.context.stroke()
-      }
-    })
-  }
   pad (num) {
     if (num < 10) {
       return '00' + num
